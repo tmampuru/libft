f1bd86efa994a966e7934bf51ddc4a5d26162ae7
Tumelo MAMPURU <tmampuru@e5r3p10.wethinkcode.co.za>
2019-02-04 17:15:18 +0200

    modified

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,8 +6,38 @@
 /*   By: tmampuru <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/03 09:55:16 by tmampuru          #+#    #+#             */
-/*   Updated: 2019/02/03 09:58:42 by tmampuru         ###   ########.fr       */
+/*   Updated: 2019/02/04 14:40:50 by tmampuru         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
 
+void	ft_atoi(char *str)
+{
+	int i;
+	int res;
+	int sign;
+
+	i = 0;
+	sign = 1;
+	res = 0;
+	while (str[i] >= '\t' && (str[i] <= '\r' || str[i] == ' '))
+		i++;
+	if (str[i] == '-')
+		sign = -1;
+	if (str[i] == '-' || str[i] == '+')
+		i++;
+	while (str[i] != '\0' && str[i] >= '0' && str[i] <= '9')
+	{
+		res = (res * 10) + (str[i] - 48);
+		i++;
+	}
+	res = res * sign;
+	write(1, &res, 1);
+}
+
+int main(void)
+{
+	ft_atoi("hello");
+	return (0);
+}
